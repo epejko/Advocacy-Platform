55bf55168f79a327c910d1aa8a1a353bee827e86
Olivia Anderson <oanders@tulane.edu>
2018-11-19 09:28:39 +0000

    add fixed categories to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
     #association to user db
-    #belongs_to :user
+    belongs_to :user
+    
+    validates_inclusion_of :category, in: Options['Categories']
     
 end
