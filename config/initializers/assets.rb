344043ac6d4b0eb8f79bc14de948d086d4ad56f6
Olivia Anderson <oanders@tulane.edu>
2018-11-25 13:07:49 +0000

    add style to learn page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( tasks.css )
 Rails.application.config.assets.precompile += %w( shared.css )
 Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( pages.css )
+Rails.application.config.assets.precompile += %w( pages.js )
