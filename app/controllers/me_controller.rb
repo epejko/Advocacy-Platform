8728ff5ebf18eafb9e79bc932d0c9eeca1d0a88e
Olivia Anderson <oanders@tulane.edu>
2018-12-06 12:57:24 +0000

    change profile to users show

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,8 +1,14 @@
 class MeController < ApplicationController
     before_action :authenticate 
     def profile
-        @user = current_user
-        @tasks = Task.where(author: current_user.username)
+        if current_user.admin?
+            @user = User.find(params[:id])
+            @users = User.all
+            @tasks_all = Task.all
+        else 
+            @user = current_user
+            @tasks = Task.where(author: @user.username)
+        end
     end
     
     
